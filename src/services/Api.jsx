3c1a9ee2253c467fd9ba634422c575a7258a0e66
Jeferson <fejedamas@hotmail.com>
2022-07-05 17:39:08 -0300

    git can't ignore services. Left api url blank for now

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 class Api {
   constructor(limit = 30) {
     this.axios = axios.create({
-      baseURL: 'postgres://xzjonuddurxevj:aefe177f2030598632424e77aa9a7ae93a7b6a2bc3f5cf297295fa36831dd889@ec2-34-227-120-79.compute-1.amazonaws.com:5432/d567ocracvdifk'
+      baseURL: ''
     });
   };
 
