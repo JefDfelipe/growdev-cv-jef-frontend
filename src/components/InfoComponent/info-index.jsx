66241687175ddfcbc3d29851f948c0ba7f5618d0
Jeferson <fejedamas@hotmail.com>
2022-07-05 18:53:13 -0300

    updated

--- src/components/InfoComponent/info-index.jsx
+++ src/components/InfoComponent/info-index.jsx
@@ -6,9 +6,9 @@ export default function Info() {
       <div title="Profile Picture" class="photo"></div>
       <h1 className="header-name">Jeferson Felipe Damas</h1>
       <section className="header-info">
-        <p>(47) 98447-2272</p>
-        <p>Rua Philipp Bauler - 1071, Testo Salto, Blumenau - SC, 89074-300, Brasil</p>
-        <p>fejedamas@hotrmail.com</p>
+        <p className="phone">47 98447-2272</p>
+        <p className="address">Rua Philipp Bauler-1071, Testo Salto, Blumenau - SC, 89074-300, Brasil</p>
+        <p className="email">fejedamas@hotrmail.com</p>
       </section>
     </header>
   );
